71e05ec1185b62ad09c37cd9b65c78ead1d4a65e
Teach-Dom <62570296+Teach-Dom@users.noreply.github.com>
2020-04-21 17:29:28 +0200

    The chat app BETA version

---
+++ js/firebase.init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB1GV4ATMhhT3sAe46GMrGCEluDAQUXY_0",
+    authDomain: "flymessages-5d639.firebaseapp.com",
+    databaseURL: "https://flymessages-5d639.firebaseio.com",
+    projectId: "flymessages-5d639",
+    storageBucket: "flymessages-5d639.appspot.com",
+    messagingSenderId: "4017677909",
+    appId: "1:4017677909:web:25797e19bf26cac13116dc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
